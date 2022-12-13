4b1b0d508af384cca14fca6e3203649f41eb3a6e
Jesus chamorro <jesusfeli54@gmail.com>
2022-12-12 21:19:21 -0500

    Default Styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NewComponent } from './new/new.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewComponent
   ],
   imports: [
     BrowserModule,
