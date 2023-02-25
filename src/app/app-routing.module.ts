80157e2c5614d5cca87d9945955c71de8625fe98
Jesus chamorro <jesusfeli54@gmail.com>
2023-02-24 22:08:33 -0500

    Creación del modulo feed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ const APP_ROUTES: Routes = [
   // Modules
   { path: 'auth', data: { preload: true, loadAfterSeconds: 1 }, loadChildren: () => import('./auth/auth.module').then(mod => mod.AuthModule), canActivate: [] },
   { path: 'fem', data: { preload: true, loadAfterSeconds: 1 }, loadChildren: () => import('./landing-pages/landin.module').then(mod => mod.LandinModule), canActivate: [] },
+  { path: 'feed', data: { preload: true, loadAfterSeconds: 1 }, loadChildren: () => import('./feed/feed.module').then(mod => mod.FeedModule), canActivate: [] },
   // { path: '', redirectTo: 'fem', pathMatch: 'full'},
   { path: '**', redirectTo: 'fem', pathMatch: 'full' },
 ];
