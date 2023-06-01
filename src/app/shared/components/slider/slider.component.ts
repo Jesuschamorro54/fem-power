02b27b21e3376457bee0e43e72b536b74095fd90
Jesus chamorro <jesusfeli54@gmail.com>
2023-06-01 06:00:50 -0300

    Nuevos cambios

--- src/app/shared/components/slider/slider.component.ts
+++ src/app/shared/components/slider/slider.component.ts
@@ -28,17 +28,17 @@ export class SliderComponent implements OnInit {
 
     this.array_slider=[
       {title: "Como mujer rural",
-       description: "Puedes ser parte de FemPower seleccionando la opción Únete e indicando el codigo que le asigne su fundación. ",
-       requirements: "Requisitos: Pertener a una fundación que se encuentre registrada en FemPower.",
+       description: "Puedes ser parte de RuralWom seleccionando la opción Únete e indicando el codigo que le asigne su fundación. ",
+       requirements: "Requisitos: Pertener a una fundación que se encuentre registrada en RuralWom.",
        img: "https://fomenta.org.co/wp-content/uploads/2022/10/ima1-blog.jpg"
       },
       {title: "Como fundación",
-       description: "Puedes ser parte de FemPower seleccionando la opción Únete y adjuntando el documento que certifica a la entidad como organizacion sin animo de lucro para mujeres rurales. ",
+       description: "Puedes ser parte de RuralWom seleccionando la opción Únete y adjuntando el documento que certifica a la entidad como organizacion sin animo de lucro para mujeres rurales. ",
        requirements: "Requisitos: Tener registrada a la entidad nacionalmente y tener el certificado.",
        img: "https://www.clarin.com/img/2020/03/10/fLGL7dv2_1200x630__1.jpg"
       },
       {title: "Como empresa",
-       description: "Puedes ser parte de FemPower seleccionando la opción Únete y adjuntando el documento que certifica a la entidad como organizacion legalmente constituida. ",
+       description: "Puedes ser parte de RuralWom seleccionando la opción Únete y adjuntando el documento que certifica a la entidad como organizacion legalmente constituida. ",
        requirements: "Requisitos: Tener registrada a la entidad, y estar dispuesta a contribuir economicamente con el progreso de las mujeres rurales en sus determinadas fundaciones.",
        img: "https://www.aaa.com.co/wp-content/uploads/2017/12/Premio-Triple-A1-2017.jpg"
       }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,4 +9,5 @@ export const environment = {
   urlAPI: 'https://fn4cn7rdv3.execute-api.us-east-1.amazonaws.com/prod',
   redirectSignIn: 'http://127.0.0.1:4000/auth/sign',
   redirectSignOut: 'http://127.0.0.1:4000/auth/login',
+  s3PublicUrl: 'https://fempower-public.s3.us-east-1.amazonaws.com/'
 };
